c9cdc0c677876bf23abc63ebb554e8ecaa08ff47
davisamis <davisamis212@gmail.com>
2026-01-19 08:54:39 -0800

    arquivos iniciais de estudo

---
+++ src/com/davisamis/maratonajava/introducao/Aula01_OlaDevDojo.java
@@ -0,0 +1,7 @@
+package com.davisamis.maratonajava.introducao;
+
+public class Aula01_OlaDevDojo {
+    public static void main(String[] args) {
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
